3315cd646f1ad0522ba7db9a8463de62069b13f9
akio0911 <akio0911@gmail.com>
2021-10-30 07:56:33 +0900

    feedback

--- Hiro42Kaiadi12/Controller/InputView.swift
+++ Hiro42Kaiadi12/Controller/InputView.swift
@@ -7,42 +7,26 @@
 
 import UIKit
 
-public enum UserDefaultKey: String {
-    case tax
-    case val
+protocol InputViewDelegate: AnyObject {
+    func didTapCalculationButton()
 }
 
 class InputView: UIView {
-    @IBOutlet private weak var amountTextField: UITextField!
-    @IBOutlet private weak var taxTextField: UITextField!
-    @IBOutlet private weak var resultLabel: UILabel!
+    @IBOutlet private(set) weak var amountTextField: UITextField!
+    @IBOutlet private(set) weak var taxTextField: UITextField!
+    @IBOutlet private(set) weak var resultLabel: UILabel!
     @IBOutlet private weak var customView: UIView!
-    override func awakeFromNib() {
-        if let loadingData = UserDefaults.standard.loading(key: .tax) {
-            taxTextField.text = loadingData
-        }
-    }
+
+    weak var delegate: InputViewDelegate?
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         Bundle.main.loadNibNamed("InputView", owner: self, options: nil)
         addSubview(customView)
         customView.frame = self.bounds
     }
-    @IBAction private func calculationButton(_ sender: Any) {
-        guard let amount = Double(amountTextField.text ?? "") else {return}
-        guard let tax = Double(taxTextField.text ?? "") else {return}
-        let result = CalculationSource().calculate(amount: Double(amount), tax: Double(tax))
-        resultLabel.text = CustomNumberFormatter.floor(number: result)
-        UserDefaults.standard.save(value: tax, key: .tax)
-    }
-}
 
-// Fixme: セッターゲッターに変更？Foilの使用?
-extension UserDefaults {
-    func save(value: Any, key: UserDefaultKey) {
-        set(value, forKey: key.rawValue)
-    }
-    func loading(key: UserDefaultKey) -> String? {
-        return string(forKey: key.rawValue)
+    @IBAction private func calculationButton(_ sender: Any) {
+        delegate?.didTapCalculationButton()
     }
 }
--- Hiro42Kaiadi12/Controller/ViewController.swift
+++ Hiro42Kaiadi12/Controller/ViewController.swift
@@ -7,6 +7,41 @@
 
 import UIKit
 
+private enum UserDefaultKey: String {
+    case tax
+    case val
+}
+
 class ViewController: UIViewController {
     @IBOutlet private weak var customView: InputView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        customView.delegate = self
+
+        if let loadingData = UserDefaults.standard.loading(key: .tax) {
+            customView.taxTextField.text = loadingData
+        }
+    }
+}
+
+extension ViewController: InputViewDelegate {
+    func didTapCalculationButton() {
+        guard let amount = Double(customView.amountTextField.text ?? "") else {return}
+        guard let tax = Double(customView.taxTextField.text ?? "") else {return}
+        let result = CalculationSource().calculate(amount: Double(amount), tax: Double(tax))
+        customView.resultLabel.text = CustomNumberFormatter.floor(number: result)
+        UserDefaults.standard.save(value: tax, key: .tax)
+    }
+}
+
+// Fixme: セッターゲッターに変更？Foilの使用?
+private extension UserDefaults {
+    func save(value: Any, key: UserDefaultKey) {
+        set(value, forKey: key.rawValue)
+    }
+    func loading(key: UserDefaultKey) -> String? {
+        return string(forKey: key.rawValue)
+    }
 }
